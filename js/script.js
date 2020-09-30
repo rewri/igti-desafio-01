60db24217e6bd8ecf34f22d086090779af3bf540
Rérisson Fumes <rkfumes@gmail.com>
2020-09-30 11:03:05 -0300

    Basic filter

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
 'use strict';
 
 const globalState = {
-    allDevs: [],
-    filteredDevs: [],
+    allDevs      : [],
+    filteredDevs : [],
     currentFilter: "",
 };
 
 const globalDivDevs    = document.querySelector("#divDevs");
-const globalInputName  = document.querySelector("#inputSearch");
-const globalCheckboxes = document.querySelector("#checkboxes");
-const globalRadioAnd   = document.querySelector("#radioAnd");
-const globalRadioOr    = document.querySelector("#radioOr");
+const globalInputName  = document.querySelector("#inputName");
+
+// const globalCheckboxes = document.querySelector("#checkboxes");
+// const globalRadioAnd   = document.querySelector("#radioAnd");
+// const globalRadioOr    = document.querySelector("#radioOr");
 
 window.addEventListener('load', () => {
 
@@ -20,63 +21,68 @@ window.addEventListener('load', () => {
         filterDevs();
     }
 
-    async function fetchAll() {
-        const url = "http://localhost:3001/devs";
-        const resource = await fetch(url);
-        const json = await resource.json();
-
-        globalState.allDevs = [...json];
-        globalState.filteredDevs = [...json];
-    }
-
     function handleInputChange({ target }) {
         globalState.currentFilter = target.value.toLocaleLowerCase().trim();
         filterDevs();
     }
 
+    async function fetchAll() {
+        const url      = "http://localhost:3001/devs";
+        const resource = await fetch(url);
+        const json     = await resource.json();
+        const jsonWithImprovedSearch = json.map((item) => {
+            const { name, programmingLanguages } = item;
+            const lowerCaseName = name.toLocaleLowerCase();
+            return {
+                ...item,
+                searchName: removeAccentMarksFrom(lowerCaseName)
+                    .split("")
+                    .filter((char) => char !== " ")
+                    .join(""),
+                searchLanguages: getOnlyLanguagesFrom(programmingLanguages)
+            };
+        });
+        globalState.allDevs = [...jsonWithImprovedSearch];
+        globalState.filteredDevs = [...jsonWithImprovedSearch];
+    }
+
     function filterDevs() {
-        // const { allDevs, radioOr, currentFilter, checkboxes } = globalState;
-        // let filteredDevs = [];
-        // if (currentFilter) {
-        //     filteredDevs = filteredDevs.filter(({ searchName }) =>
-        //         searchName.includes(currentFilter)
-        //     );
-        // }
-        // globalState.filteredDevs = filteredDevs;
+        const { allDevs, currentFilter } = globalState;
+        let filteredDevs = allDevs;
+        if (currentFilter) {
+            filteredDevs = allDevs.filter(({ searchName }) =>
+                searchName.includes(currentFilter)
+            );
+        }
+        globalState.filteredDevs = filteredDevs;
         renderDevs();
     }
 
     function renderDevs() {
-
         const { filteredDevs } = globalState;
-
-        const devsToShow = filteredDevs
-            .map((dev) => {
-                return renderDev(dev);
-            })
-            .join("");
-
+        const devsToShow = filteredDevs.map((dev) => {
+            return renderDev(dev);
+        }).join("");
         const renderedHTML = `
-            <div class="col-xs-12">
-                <h2>${filteredDevs.length} (s) encontrado(s)</h2>
-            </div>       
-            <div>
+            <div class="row">
+                <div class="col-xs-12">
+                    <h2>${filteredDevs.length} (s) encontrado(s)</h2>
+                </div>
                 ${devsToShow}
-            </div>
+            </div>    
         `;
         globalDivDevs.innerHTML = renderedHTML;
     }
 
     function renderDev(dev) {
-        const { id, name, picture, programmingLanguages } = dev;
-
+        const { name, picture, programmingLanguages } = dev;
         return `
             <div class='col-xs-3 dev-card'>
                 <img class='profile' src="${picture}" alt="${name}" />
                 <div class='data'>
                     <span>${name}</span>
                     <span class='programmingLanguages'>
-                        
+                        ${renderProgrammingLanguages(programmingLanguages)}
                     </span>
                 </div>
             </div>
@@ -84,12 +90,29 @@ window.addEventListener('load', () => {
     }
 
     function renderProgrammingLanguages(languages) {
-        const { checkboxes } = globalState;
-        return languages
-            .map((language) => {
-                return checkboxes.find((item) => item.filter === language).language;
-            })
-            .join(", ");
+        return languages.map((language) => {
+            return language.language;
+        }).join(", ");
+    }
+
+    function removeAccentMarksFrom(text) {
+        const WITH_ACCENT_MARKS = "áãâäàéèêëíìîïóôõöòúùûüñ".split("");
+        const WITHOUT_ACCENT_MARKS = "aaaaaeeeeiiiiooooouuuun".split("");
+        const newText = text
+            .toLocaleLowerCase()
+            .split("")
+            .map((char) => {
+                 const index = WITH_ACCENT_MARKS.indexOf(char);
+                if (index > -1) {
+                    return WITHOUT_ACCENT_MARKS[index];
+                }
+                return char;
+            }).join("");
+        return newText;
+    }
+
+    function getOnlyLanguagesFrom(languages) {
+        return languages.map((language) => language.language.toLocaleLowerCase()).sort();
     }
 
     start();
