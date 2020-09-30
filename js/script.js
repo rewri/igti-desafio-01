e1893bf1e94da9e792a669e6239e1448e215999c
Rérisson Fumes <rkfumes@gmail.com>
2020-09-30 08:00:51 -0300

    Show devs

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
 'use strict';
 
-const globalDivDevs = document.querySelector("#divDevs");
+const globalState = {
+    allDevs: [],
+    filteredDevs: [],
+    currentFilter: "",
+};
+
+const globalDivDevs    = document.querySelector("#divDevs");
+const globalInputName  = document.querySelector("#inputSearch");
+const globalCheckboxes = document.querySelector("#checkboxes");
+const globalRadioAnd   = document.querySelector("#radioAnd");
+const globalRadioOr    = document.querySelector("#radioOr");
 
 window.addEventListener('load', () => {
 
     async function start() {
+        globalInputName.addEventListener("input", handleInputChange);
         await fetchAll();
+        filterDevs();
     }
 
     async function fetchAll() {
@@ -13,22 +25,73 @@ window.addEventListener('load', () => {
         const resource = await fetch(url);
         const json = await resource.json();
 
-        return json;
+        globalState.allDevs = [...json];
+        globalState.filteredDevs = [...json];
+    }
+
+    function handleInputChange({ target }) {
+        globalState.currentFilter = target.value.toLocaleLowerCase().trim();
+        filterDevs();
+    }
+
+    function filterDevs() {
+        // const { allDevs, radioOr, currentFilter, checkboxes } = globalState;
+        // let filteredDevs = [];
+        // if (currentFilter) {
+        //     filteredDevs = filteredDevs.filter(({ searchName }) =>
+        //         searchName.includes(currentFilter)
+        //     );
+        // }
+        // globalState.filteredDevs = filteredDevs;
+        renderDevs();
     }
 
     function renderDevs() {
+
+        const { filteredDevs } = globalState;
+
+        const devsToShow = filteredDevs
+            .map((dev) => {
+                return renderDev(dev);
+            })
+            .join("");
+
         const renderedHTML = `
             <div class="col-xs-12">
-                <h2>dev(s) encontrado(s)</h2>
+                <h2>${filteredDevs.length} (s) encontrado(s)</h2>
             </div>       
-            <div class="col-xs-12">
-                render
+            <div>
+                ${devsToShow}
             </div>
         `;
         globalDivDevs.innerHTML = renderedHTML;
     }
 
-    renderDevs();
+    function renderDev(dev) {
+        const { id, name, picture, programmingLanguages } = dev;
 
+        return `
+            <div class='col-xs-3 dev-card'>
+                <img class='profile' src="${picture}" alt="${name}" />
+                <div class='data'>
+                    <span>${name}</span>
+                    <span class='programmingLanguages'>
+                        
+                    </span>
+                </div>
+            </div>
+        `;
+    }
+
+    function renderProgrammingLanguages(languages) {
+        const { checkboxes } = globalState;
+        return languages
+            .map((language) => {
+                return checkboxes.find((item) => item.filter === language).language;
+            })
+            .join(", ");
+    }
+
+    start();
 
 });
